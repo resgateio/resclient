9bf25d029a74fa02cf4b42d741c5708643b86eb0
Samuel Jirénius <samuel@jirenius.com>
2018-06-06 17:17:42 +0200

    Improved error handling. ResError now includes RID if possible.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
  * Client implementation of the RES-Client Protocol.
  */
 
-import ResClient from './class/ResClient.js';
+import ResClient, { isResError } from './class/ResClient.js';
 import ResCollection from './class/ResCollection.js';
 import ResModel from './class/ResModel.js';
 
-export { ResCollection, ResModel };
+export { ResCollection, ResModel, isResError };
 export default ResClient;
