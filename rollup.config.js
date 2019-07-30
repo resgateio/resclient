7d424c8b992bd223e88e570def30b7c6d91e614f
Samuel Jirénius <samuel@jirenius.com>
2019-07-30 09:56:47 +0200

    GH-26: npm update and updated rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -12,9 +12,7 @@ export default {
 	},
 	plugins: [
 		resolve({
-			jsnext: true,
-			main: true,
-			browser: true,
+			mainFields: [ 'jsnext:main', 'main', 'browser' ]
 		}),
 		babel({
 			exclude: 'node_modules/**'
