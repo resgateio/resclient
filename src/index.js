8ac78969659848afc4e9887b520884e92e5e166c
Christofer Engel <christofer.engel@gmail.com>
2022-05-18 07:35:34 +0200

    Export ResError, make d.ts file.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * Client implementation of the RES-Client Protocol.
  */
 
-export { default, isResError } from './class/ResClient.js';
+export { default as ResClient, isResError } from './class/ResClient.js';
 export { default as ResCollection } from './class/ResCollection.js';
 export { default as ResModel } from './class/ResModel.js';
+export { default as ResError } from './class/ResError.js';
